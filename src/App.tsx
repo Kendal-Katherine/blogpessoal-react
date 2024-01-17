16cdb765cd18f57e6f08079b00d44d680d8e886e
Kendal Katherine <kendal.katherine3@gmail.com>
2024-01-17 20:49:28 -0300

    Utilização do Tailwild para estilizar a página

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Home from './pages/home/home.tsx'
+import Footer from './components/footer/Footer.tsx'
+import Navbar from './components/navbar/Navbar.tsx'
+import Home from './pages/home/Home.tsx'
+import Login from './pages/login/Login.tsx'
 
 function App() {
-  
+
   return (
     <>
-      <Home />
-      <Home />
-   </>
+      <BrowserRouter>
+      <Navbar/>
+      <div className='min-h-[80vh]'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          
+
+        </Routes>
+
+      </div>
+      <Footer/>
+      </BrowserRouter>    
+    </>
   )
 }
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,33 +1,40 @@
+import { useNavigate, Link } from "react-router-dom"
+
 
 function Home() {
-
+    const navigate = useNavigate()
     return (
+      
         <>
-            <div id="container" style={{
-                width: "100vw",
-                display: 'flex',
-                justifyContent: "center"
-            }}>
-                <div id="subcontainer">
-                    <div id="texto" style={{
-                        width: "80vw",
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: "center"
-                    }} >
-                        <h2>Seja Bem Vinde</h2>
-                        <p>Expresse aqui os seus pensamentos</p>
+            <div id="container" className="bg-pink-500 flex justify-center">
+                <div id="subcontainer" className="container grid grid-cols-2 text-white">
+                    <div id="texto" className="flex flex-col gap-4 items-center justify-center py-4" >
+                        <h2 className="
+                        text-5xl
+                        font-bold">Seja Bem Vinde</h2>
+                        <p className="text-xl">Expresse aqui os seus pensamentos</p>
+                        
+                        <div className="flex justify-around gap-4">
+                            <div className="rounded text-white border-white border-solid border-2 px-4 py-2">
+                                Nova Postagem
+                            </div>
+                        </div>
+
                     </div>
-                    <div id="imagem" style={{
-                        width: "80vw",
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: "center"
-                    }}>
-                        <img src="https://i.imgur.com/VpwApCU.png"
+                    <div id="imagem" className="flex justify-center">
+                        <img src="src\assets\img\static-assets-animate.svg"
                             alt="Imagem da Página"
-                            width="400px" />
+                            className="w-2/3"/>
                     </div>
+
+                    <div>
+                    <button type="submit"
+                        onClick={() => { navigate('/login') }}
+                        className="hover:underline mx-4">
+                        Login useNavigate
+                    </button>
+                    <Link to='/login' className="hover:underline mx-4">Login por Link</Link>
+                </div>
                 </div>
             </div >
         </>
